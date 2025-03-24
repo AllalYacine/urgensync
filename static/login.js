045854d546099772f019427bc38a37faf93441a5
Yacine Allal <schoolboyyacine@gmail.com>
2025-03-24 23:18:32 +0100

    added an error message div

--- static/login.js
+++ static/login.js
@@ -1,3 +1,5 @@
+const { setTimeout } = require("timers");
+
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
 
@@ -7,7 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
         return emailRegex.test(email);
     }
 
-    
+    // if there is an error, we create an error message and show it in the input field 
+    const showError = (input, message) => {
+        const formInfo = input.parentElement;
+        const errorDiv = document.createElement('div');
+        errorDiv.className = 'error-message';
+        errorDiv.style.color = '#ff3333'
+        errorDiv.style.fontSize = '0.8em';
+        errorDiv.style.marginTop = '5px';
+        errorDiv.textContent = message;
 
+        formInfo.appendChild(errorDiv);
+        input.style.borderColor= "#ff3333";
+
+        // after 3 seconds, the error disappears, you can change the time as you like
+        setTimeout(() => {
+            errorDiv.remove();
+            input.style.borderColor = '#ddd';
+        }, 3000);
+    };
+
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault(); // i added this to prevent the reload of the page after submission
+        const email = loginForm.querySelector('input[type="email"]');
+        const password = loginForm.querySelector('input[type="password"]');
+
+        if(!validateEmail(email.value)) {
+            showError(email, 'Please enter a valid email');
+            return;
+        }
+
+    })
 
 });
